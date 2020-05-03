559287a643a4828890ee575347ff1dc596c3f7ff
BalaK <balakishore.pothuluri@gmail.com>
2020-05-03 00:23:07 -0500

    changes to repo

--- src/main/java/com/example/kishore/demo/DemoApplication.java
+++ src/main/java/com/example/kishore/demo/DemoApplication.java
@@ -3,13 +3,15 @@ package com.example.kishore.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 
+ */
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
 
-        // Commit testing
     }
 
 }
